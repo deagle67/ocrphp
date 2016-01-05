fc3a1c6e19cca0237ec3070733e2e1bb9f7e94e7
deagle67 <deagle67@hotmail.com>
2016-01-05 03:27:47 +0100

    1and1

---
+++ app/config/prod.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Doctrine (db)
+$app['db.options'] = array(
+	'driver'   => 'pdo_mysql',
+	'charset'  => 'utf8',
+	'host'     => 'db607839198.db.1and1.com',
+	'port'     => '3306',
+	'dbname'   => 'db607839198',
+	'user'     => 'dbo607839198',
+	'password' => 'Func10301337',
+);
